9756bfbb3ad4957dbb46d27344e626ccea005c99
Iwo Błądek <iwo.bladek@cs.put.poznan.pl>
2020-04-09 18:54:41 +0200

    Lab3: reduce redundant code, remove potentially problematic 'test_*' function names.

--- lab3/zadania_eigen.py
+++ lab3/zadania_eigen.py
@@ -59,43 +59,30 @@ def plot_attractors(A, vectors):
     pass
 
 
-def test_A1(vectors):
-    """Standard scaling transformation."""
-    A = np.array([[2, 0],
-                  [0, 2]])
+def show_eigen_info(A, vectors):
     EVD_decomposition(A)
     visualize_transformation(A, vectors)
     plot_attractors(A, vectors)
-
-
-def test_A2(vectors):
-    A = np.array([[-1, 2],
-                  [2, 1]])
-    EVD_decomposition(A)
-    visualize_transformation(A, vectors)
-    plot_attractors(A, vectors)
-
-
-def test_A3(vectors):
-    A = np.array([[3, 1],
-                  [0, 2]])
-    EVD_decomposition(A)
-    visualize_transformation(A, vectors)
-    plot_attractors(A, vectors)
-
-
-def test_A4(vectors):
-    A = np.array([[2, -1],
-                  [1, 4]])
-    EVD_decomposition(A)
-    visualize_transformation(A, vectors)
-    plot_attractors(A, vectors)
-
 
 
 if __name__ == "__main__":
     vectors = vectors_uniform(k=8)
-    test_A1(vectors)
-    test_A2(vectors)
-    test_A3(vectors)
-    test_A4(vectors)
+
+    A = np.array([[2, 0],
+                  [0, 2]])
+    show_eigen_info(A, vectors)
+
+
+    A = np.array([[-1, 2],
+                  [2, 1]])
+    show_eigen_info(A, vectors)
+
+
+    A = np.array([[3, 1],
+                  [0, 2]])
+    show_eigen_info(A, vectors)
+
+
+    A = np.array([[2, -1],
+                  [1, 4]])
+    show_eigen_info(A, vectors)
