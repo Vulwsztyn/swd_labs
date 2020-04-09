af140a8ce7775ec0d5babc71383623b331f2f3f0
Iwo Błądek <iwo.bladek@cs.put.poznan.pl>
2020-04-09 18:38:36 +0200

    Lab1: add information about encoding

--- lab1/matplotlib_template.py
+++ lab1/matplotlib_template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D # Without this projection='3d' is not recognized
